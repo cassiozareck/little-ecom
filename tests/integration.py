632781b51308221fadedb368385d6af036bbdf32
cassiozareck <cassiomilczareck@gmail.com>
2023-11-22 11:59:12 -0300

    Improving integration tests

--- tests/integration.py
+++ tests/integration.py
@@ -31,6 +31,26 @@ class APITestClient:
         assert response.status_code == 200
         return response.json()
 
+    def update_item(self, item_id, item_data):
+        headers = {'Authorization': f'Bearer {self.token}'}
+        response = requests.put(f'{self.BASE_URL}/item/{item_id}', json=item_data, headers=headers)
+        print("update_item: ", response, response.text)
+        assert response.status_code == 200
+
+    def get_items(self):
+        headers = {'Authorization': f'Bearer {self.token}'}
+        response = requests.get(f'{self.BASE_URL}/items', headers=headers)
+        print("get_items: ", response, response.text)
+        assert response.status_code == 200
+        return response.json()
+
+    def get_items_by_owner(self, owner):
+        headers = {'Authorization': f'Bearer {self.token}'}
+        response = requests.get(f'{self.BASE_URL}/items/{owner}', headers=headers)
+        print("get_items_by_owner: ", response, response.text)
+        assert response.status_code == 200
+        return response.json()
+
     # should pass the account to be deleted in body (email and password)
     def delete_account(self, account):
         response = requests.delete(f'{self.BASE_URL}/auth/delete', json=account)
@@ -43,7 +63,13 @@ class APITestClient:
         print("remove_item: ", response, response.text)
         assert response.status_code == 200
 
+# Basic test suite that will register a user, add an item, update the item, and
+# delete the item and user, not making any mistake.
 def test_suite1():
+    print()
+    print("TEST SUITE 1: --------")
+    print()
+
     client = APITestClient()
     test_user = {
         "email": "testowner@example.com",
@@ -54,16 +80,80 @@ def test_suite1():
         "owner": "testowner",
         "price": 9.99
     }
-    id = None
+
     try:
         client.register(test_user)
         client.signin(test_user)
         id = client.add_item(test_item)
-        print(id)
         item = client.get_item(id)
-        print(item)
+        item["price"] = 19.99
+        client.update_item(id, item)
+        item = client.get_item(id)
+        assert item["name"] == "Test Item"
+        assert item["owner"] == "testowner"
+        assert item["price"] == 19.99
+
     finally:
         client.remove_item(id)
         client.delete_account(test_user)
 
-test_suite1()
+
+# This test suite will add two items and check if get_items endpoints return
+# the correct items.
+def test_suite2():
+    print()
+    print("TEST SUITE 2: --------")
+    print()
+
+    client = APITestClient()
+    test_user = {
+        "email": "megy@gmail.com",
+        "password": "securepassword"
+    }
+    test_item = {
+        "name": "apple",
+        "owner": "megy",
+        "price": 0.99
+    }
+    test_item2 = {
+        "name": "pineapple",
+        "owner": "megy",
+        "price": 1.99
+    }
+
+    try:
+        client.register(test_user)
+        client.signin(test_user)
+        id = client.add_item(test_item)
+        id2 = client.add_item(test_item2)
+        items = client.get_items()
+        items2 = client.get_items_by_owner("megy")
+
+        # Check if all items in items2 are in items
+        assert all(item in items for item in items2), "Not all items from items2 are in items"
+
+    finally:
+        client.remove_item(id)
+        client.remove_item(id2)
+        client.delete_account(test_user)
+
+# This test suite will try to register a user with an invalid email and password
+def test_suite3():
+    print()
+    print("TEST SUITE 3: --------")
+    print()
+
+    client = APITestClient()
+    test_user = {
+        "email": "",
+        "password": ""
+    }
+    try:
+        client.register(test_user)
+    except AssertionError:
+        print("Response expected")
+
+if __name__ == '__main__':
+    test_suite1()
+    test_suite2()
+    test_suite3()
